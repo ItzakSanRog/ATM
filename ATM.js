253182afcb02d56139069eef91a36a0800aa0c53
Edgar Itzak Sanchez Rogers <46898741+ItzakSanRog@users.noreply.github.com>
2019-03-12 15:17:05 -0600

    Add files via upload

--- ATM.js
+++ ATM.js
@@ -1,27 +1,54 @@
-var cantidadInput = document.getElementById("inputCantidad");
-var boton = document.getElementById("botonCalcular")
+var inputCantidad = document.getElementById("inputCantidad");
+var botonCalcular = document.getElementById("botonCalcular");
+var parrafo = document.createElement("P");
 
 
-boton.addEventListener("click",calcular);
 
+botonCalcular.addEventListener("click", calcular);
 
+var cantidadBilletes;
 var billetesDisponibles = [];
 var billetesAEntregar = [];
 
 
-billetesDisponibles.push(new Billete(500,5));
-billetesDisponibles.push(new Billete(200,3));
-billetesDisponibles.push(new Billete(100,4));
-billetesDisponibles.push(new Billete(50,5));
-billetesDisponibles.push(new Billete(20,2));
-billetesDisponibles.push(new Billete(10,4));
+billetesDisponibles.push(new Billete(500, 8));
+billetesDisponibles.push(new Billete(200, 3));
+billetesDisponibles.push(new Billete(100, 2));
+billetesDisponibles.push(new Billete(50, 2));
 
 
-function calcular(){
-    for(var i in billetesDisponibles){
-        if(cantidadInput.value =! 0){
-            billetes
+
+function calcular() {
+    var montoSolicitado = parseInt(inputCantidad.value);
+    for (var i in billetesDisponibles) {
+        if (inputCantidad.value > 0) {
+            cantidadBilletes = parseInt(montoSolicitado / billetesDisponibles[i].valor);
+            if(cantidadBilletes>billetesDisponibles[i].cantidad){
+                cantidadBilletes = billetesDisponibles[i].cantidad;
+            }
+            montoSolicitado= montoSolicitado-(cantidadBilletes*billetesDisponibles[i].valor);
+            billetesAEntregar.push(new Billete(billetesDisponibles[i].valor,cantidadBilletes));
         }
-        cantidadInput.value
+        
+    }
+    if(montoSolicitado==0){
+        mostrar();
+    }else{
+        
+        var text = document.createTextNode("No puedo entregarte esa cantidad");
+        parrafo.appendChild(text);
+        document.getElementById("output").appendChild(parrafo);
+        
+    }
+}
+
+function mostrar(){
+    for(var i in billetesAEntregar){
+        
+        console.log(billetesAEntregar[i]);
+        var text = document.createTextNode(billetesAEntregar[i].cantidad+" billete(s) de "+billetesAEntregar[i].valor+"__");
+        parrafo.appendChild(text);
+        document.getElementById("output").appendChild(parrafo);
+
     }
 }
